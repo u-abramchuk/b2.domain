4500b57d2af161ed86ae89d9825fc387b7ae2625
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-08-11 18:32:36 +0300

    use named docker containers

--- scripts/development.bat
+++ scripts/development.bat
@@ -4,5 +4,5 @@ popd
 pushd ..\src\b2.Domain.Web
     start dotnet watch run
 popd
-start docker run -it -p 2113:2113 -p 1113:1113 eventstore/eventstore
-start docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+start docker run -it --name b2-eventstore -p 2113:2113 -p 1113:1113 eventstore/eventstore
+start docker run -d --hostname my-rabbit --name b2-rabbitmq -p 15672:15672 -p 5672:5672 rabbitmq:3-management
