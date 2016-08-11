5ee759542401f6e9d0f44006af9d233a60d75822
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-08-11 18:23:16 +0300

    make development.bat to lauch necessary docker containers

--- scripts/development.bat
+++ scripts/development.bat
@@ -4,3 +4,5 @@ popd
 pushd ..\src\b2.Domain.Web
     start dotnet watch run
 popd
+start docker run -it -p 2113:2113 -p 1113:1113 eventstore/eventstore
+start docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
