da453cb21e327079fbc216ff6b4acfdedd8ccc7f
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-09-19 19:05:40 +0300

    fix rabbitmq bindings

--- src/b2.Domain.Web/EventPublisher.cs
+++ src/b2.Domain.Web/EventPublisher.cs
@@ -24,18 +24,21 @@ namespace b2.Domain.Web
             using (var connection = _factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
+                channel.ExchangeDeclare(exchange: "b2.domain.events", type: "topic", durable: true);
                 channel.QueueDeclare(queue: "b2.domain.events",
                                                  durable: true,
                                                  exclusive: false,
                                                  autoDelete: false,
                                                  arguments: null);
+                channel.QueueBind("b2.domain.events", "b2.domain.events", "b2.domain.events");
+
                 foreach (var @event in events)
                 {
                     var message = JsonConvert.SerializeObject(@event);
                     var body = Encoding.UTF8.GetBytes(message);
 
                     channel.BasicPublish(
-                        exchange: string.Empty,
+                        exchange: "b2.domain.events",
                         routingKey: "b2.domain.events",
                         basicProperties: null,
                         body: body
